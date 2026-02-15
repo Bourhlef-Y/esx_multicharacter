c80deb3702d4e5b958808b9aad018fa64ecf5970
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2026-02-15 04:50:34 +0100

    first commit

---
+++ html/locales/en.js
@@ -0,0 +1,9 @@
+const translate = new Object();
+
+translate.name = "Nom"
+translate.job = "Métier"
+translate.bank = "Banque"
+translate.money = "Espèces"
+translate.gender = "Genre"
+translate.dob = "Date de naissance"
+
